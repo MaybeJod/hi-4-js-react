605feb5a55999bd499e1599503062ee896064107
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-01-17 20:21:18 +0100

    a9 done

--- react-assignments/src/Components/MyStateComponent/MyStateComponent.jsx
+++ react-assignments/src/Components/MyStateComponent/MyStateComponent.jsx
@@ -1,12 +1,59 @@
 import React, { useState } from "react";
 
 const MyStateComponent = () => {
-  // TODO create a form using useState hook, the form should have two input fields, one for name and one for age, and a submit button and handle form data. Submit/console.log the form data.
-  return (
-    <div>
-      <h1>My State Component</h1>
-    </div>
-  );
+	// TODO create a form using useState hook, the form should have two input fields, one for name and one for age, and a submit button and handle form data. Submit/console.log the form data.
+	const [formData, setFormData] = useState({ name: "", age: "" });
+	const [isSubmitted, setIsSubmitted] = useState(false);
+
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setIsSubmitted(true);
+		console.log(`Form submitted with input:`, formData);
+	};
+
+	const renderData = () => {
+		return (
+			<p>
+				your name is: {formData.name} and you are {formData.age} years old
+			</p>
+		);
+	};
+	return (
+		<div>
+			<h2>My State Component</h2>
+			<form onSubmit={handleSubmit}>
+				<label>
+					Name
+					<input
+						type="text"
+						name="name"
+						value={formData.name}
+						onChange={handleInputChange}
+						placeholder="name"
+					/>
+				</label>
+
+				<label>
+					Age
+					<input
+						type="number"
+						name="age"
+						value={formData.age}
+						onChange={handleInputChange}
+						placeholder="age"
+					/>
+				</label>
+
+				<button type="submit">Submit</button>
+			</form>
+			{isSubmitted ? renderData() : null}
+		</div>
+	);
 };
 
 export default MyStateComponent;
